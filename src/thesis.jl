c11e9ea74a91cbc0e23b9e495fff32314662ca4d
Rupert Ettrich <rupert.ettrich@gmail.com>
2022-09-07 00:47:36 +0200

    include modules in main module

--- src/thesis.jl
+++ src/thesis.jl
@@ -1,10 +1,12 @@
 module thesis
 
-export Instances, LookaheadSearch
+export Instances, LookaheadSearch, Training, GNNs
 
 include("Instances.jl")
 # include("MPModels.jl")
 include("LookaheadSearch.jl")
 include("LocalSearch.jl")
+include("Training.jl")
+include("GNNs.jl")
 
 end # module
