b90732d3546b5e2e416fc2e9238fa86fcbfbf8e8
Rupert Ettrich <rupert.ettrich@gmail.com>
2022-11-13 15:17:17 +0100

    update mqcp run script

--- src/mqcp.jl
+++ src/mqcp.jl
@@ -11,108 +11,26 @@ using DataFrames
 using Flux
 using BSON
 using GraphNeuralNetworks
+using MHLib
+
 
 # To run, provide a graph instance and target density γ, e.g.:
 # julia --project=. .\src\mqcp.jl --graph="inst/DIMACS/brock400_1.clq" --gamma=0.999 --timelimit=60
 
+include("mqcp_args.jl")
 
-settings = ArgParseSettings()
+parse_settings!([settings_cfg, thesis.NodeRepresentationLearning.settings_cfg],
+                vcat(ARGS,
+                [
+                    #"--debug=true"
+                ]))
 
-@add_arg_table settings begin
-    "--graph"
-        help = "Graph instance"
-        arg_type = String
-        required = true
-        default = "random"
-    "--gamma"
-        help = "Target density γ"
-        arg_type = Float64
-        required = true
-    "--stm"
-        help = "Short term memory type ∈ ['tl', 'cc']"
-        arg_type = String
-        default = "tl"
-    "--alpha"
-        help = "GRASP parameter for construction heuristic, 0 ≤ α ≤ 1"
-        arg_type = Float64
-        default = 0.2
-    "--beta"
-        help = "Beamwidth of lower bound heuristic"
-        arg_type = Int
-        default = 5
-    "--expansion_limit"
-        help = "Limit expansion of nodes in beam search construction"
-        arg_type = Int
-        default = 50
-    "--p"
-        help = "Controls how much rarely visited vertices are preferred in construction heuristic, 0 ≤ p ≤ 1"
-        arg_type = Float64
-        default = 0.4
-    "--timelimit"
-        help = "Sets timelimit for execution"
-        arg_type = Float64
-        default = 1000.0
-    "--max_iter"
-        help = "Search is restarted after max_iter iterations without finding an improving solution"
-        arg_type = Int
-        default = 4000
-    "--next_improvement"
-        help = "If true: Search neighborhoods with next improvement strategy, otherwise use best improvement"
-        arg_type = Bool
-        default = true
-    "--debug"
-        help = "Enables debug output"
-        arg_type = Bool
-        default = false
-    "--write_result"
-        help = "Write result to file if directory is specified"
-        arg_type = String
-        default = "-"
-    "--scoring_function"
-        help = "Load scoring function (GNN) from file"
-        arg_type = String
-        default = "-"
-    "--vnd_depth"
-        help = "Depth d of variable neighborhood search"
-        arg_type = Int
-        default = 1
+if settings[:debug]
+    ENV["JULIA_DEBUG"] = "thesis"
 end
 
-parsed_args = parse_args(
-    [
-        ARGS..., 
-        "--graph=inst/DIMACS/brock400_1.clq",
-        "--gamma=0.999",
-        # "--scoring_function=random",
-        "--debug=true",
-        "--vnd_depth=2",
-        "--max_iter=50",
-        "--timelimit=60.0",
-    ], 
-    settings)
-
-function get_stm(stm_string)
-    if stm_string == "tl"
-        return TabuList
-    elseif stm_string == "cc"
-        return ConfigurationChecking
-    else
-        error("unknown short term memory type")
-    end
-end
-
-function run_mqcp(scoring_function=nothing; parsed_args)
-
-    if parsed_args["debug"]
-        ENV["JULIA_DEBUG"] = "thesis"
-    end
-
-    if parsed_args["graph"] == "random"
-        graph = generate_instance(200, 0.5)
-    else
-        graph = load_instance("$(parsed_args["graph"])")
-    end
-    γ = parsed_args["gamma"]
+function run_mqcp(scoring_function=nothing)
+    γ = settings[:gamma]
 
     # initialize components of local search based metaheuristic
     
@@ -121,18 +39,17 @@ function run_mqcp(scoring_function=nothing; parsed_args)
     # lower_bound_heuristic = BeamSearch_LowerBoundHeuristic(guidance_func; β=parsed_args["beta"], γ, expansion_limit=parsed_args["expansion_limit"])
     lower_bound_heuristic = SingleVertex_LowerBoundHeuristic()
 
-    construction_heuristic = Freq_GRASP_ConstructionHeuristic(parsed_args["alpha"], parsed_args["p"])
+    construction_heuristic = Freq_GRASP_ConstructionHeuristic(settings[:alpha], settings[:p])
 
-    Stm_Type = get_stm(parsed_args["stm"])
+    Stm_Type = get_stm(settings[:stm])
     short_term_memory = Stm_Type()
 
     feasibility_checker = MQCP_FeasibilityChecker(γ)
     
     solution_extender = MQCP_GreedySolutionExtender(γ)
 
-    neighborhood_search = VariableNeighborhoodDescent(parsed_args["vnd_depth"])
+    neighborhood_search = VariableNeighborhoodDescent(settings[:vnd_depth])
 
-    # 
     if isnothing(scoring_function)
         scoring_function = d_S_ScoringFunction()
     end
@@ -141,56 +58,45 @@ function run_mqcp(scoring_function=nothing; parsed_args)
     local_search_procedure = MQCP_LocalSearchProcedure(γ, short_term_memory, scoring_function)
     println("ScoringFunction: $(typeof(scoring_function))")
 
-    timelimit = parsed_args["timelimit"]
-    max_iter = parsed_args["max_iter"]
-    next_improvement = parsed_args["next_improvement"]
-    record_swap_history = false
 
     local_search = LocalSearchBasedMH(
             lower_bound_heuristic, construction_heuristic, local_search_procedure, feasibility_checker, solution_extender;
-            neighborhood_search, timelimit, max_iter, next_improvement, record_swap_history)
+            neighborhood_search, timelimit=settings[:timelimit], max_iter=settings[:max_iter], 
+            next_improvement=settings[:next_improvement], record_swap_history=false, max_restarts=settings[:max_restarts],
+            sparse_evaluation=settings[:sparse_evaluation],
+            )
+    
+    results = Dict()
 
-    println("Settings: ")
-    for (arg,val) in parsed_args
-        println("  $arg  =>  $val")
-    end
-    solutions = []
-    for i = 1:1
-        solution, swap_history = run_lsbmh(local_search, graph)
-        push!(solutions, solution)
-        println(solution)
-        println("size of solution: $(length(solution))")
+    for inst in settings[:graph]
+        results[inst.name] = Int[]
+        for i = 1:settings[:runs_per_instance]
+            println("Start run $i/$settings[:runs_per_instance] for $(inst.name)")
+            t = @elapsed solution, _ = run_lsbmh(local_search, inst.graph)
+            push!(results[inst.name], length(solution))
+            println("Found solution: $(sort(solution)), runtime $t")
+        end
+        println("Best: $(maximum(results[inst.name])), Avg: $(sum(results[inst.name]) / length(results[inst.name]))")
+        println()
     end
 
-    if parsed_args["write_result"] != "-"
-        df = DataFrame(GraphID=String[], V=Int[], E=Int[], Dens=Real[], γ=Real[], Result=Real[])
-        push!(df, (
-            parsed_args["graph"],
-            nv(graph),
-            ne(graph),
-            density(graph),
-            γ,
-            length(sum(solutions) / length(solutions)))
-        )
-        CSV.write("$(parsed_args["write_result"])/$(parsed_args["scoring_function"])-$(split(parsed_args["graph"], "/")[end]).csv", df)
+    if settings[:write_result] != "-"
+        df = DataFrame(GraphID=String[], V=Int[], E=Int[], Dens=Real[], γ=Real[], Avg=Real[], Best=Int[])
+        for (graph_id, sols) in results
+            push!(df, (
+                graph_id,
+                nv(graph),
+                ne(graph),
+                density(graph),
+                γ,
+                length(sum(sols) / length(sols))),
+                maximum(sols)
+            )
+        end
+        
+        CSV.write("$(settings[:write_result])/$(settings[:scoring_function]).csv", df)
     end
 
 end
 
-scoring_function = nothing
-if parsed_args["scoring_function"] != "-"
-    if parsed_args["scoring_function"] == "random"
-        println("Using random scoring function")
-        scoring_function = Random_ScoringFunction(20)
-    elseif parsed_args["scoring_function"] == "encoder-decoder"
-        gnn = Encoder_Decoder_GNNModel([64, 64, 64], [32, 32])
-        scoring_function = Encoder_Decoder_ScoringFunction(gnn, 20)
-        BSON.@save "test-gnn.bson" gnn
-    else
-        println("Load scoring function $(parsed_args["scoring_function"])")
-        BSON.@load parsed_args["scoring_function"] gnn
-        scoring_function = SimpleGNN_ScoringFunction(gnn, 20)
-    end
-end
-
-run_mqcp(scoring_function; parsed_args)
+run_mqcp(scoring_function)
---
+++ src/mqcp_args.jl
@@ -0,0 +1,98 @@
+settings_cfg = ArgParseSettings()
+
+@add_arg_table! settings_cfg begin
+    "--graph"
+        help = "Vector of paths to graph instances, separated by comma"
+        arg_type = Vector
+        required = true
+    "--gamma"
+        help = "Target density γ"
+        arg_type = Float64
+        required = true
+    "--stm"
+        help = "Short term memory type ∈ ['tl', 'cc']"
+        arg_type = String
+        default = "tl"
+    "--alpha"
+        help = "GRASP parameter for construction heuristic, 0 ≤ α ≤ 1"
+        arg_type = Float64
+        default = 0.2
+    "--beta"
+        help = "Beamwidth of lower bound heuristic"
+        arg_type = Int
+        default = 5
+    "--expansion_limit"
+        help = "Limit expansion of nodes in beam search construction"
+        arg_type = Int
+        default = 50
+    "--p"
+        help = "Controls how much rarely visited vertices are preferred in construction heuristic, 0 ≤ p ≤ 1"
+        arg_type = Float64
+        default = 0.4
+    "--timelimit"
+        help = "Sets timelimit for execution"
+        arg_type = Float64
+        default = 1000.0
+    "--max_iter"
+        help = "Search is restarted after max_iter iterations without finding an improving solution"
+        arg_type = Int
+        default = 4000
+    "--max_restarts"
+        help = "Execution stops after max_restarts restarts of the local search."
+        arg_type = Int
+        default = 100
+    "--next_improvement"
+        help = "If true: Search neighborhoods with next improvement strategy, otherwise use best improvement"
+        arg_type = Bool
+        default = true
+    "--debug"
+        help = "Enables debug output"
+        arg_type = Bool
+        default = false
+    "--write_result"
+        help = "Write result to file if directory is specified"
+        arg_type = String
+        default = "-"
+    "--scoring_function"
+        help = "Load scoring function (GNN) from file"
+        arg_type = String
+        default = "-"
+    "--vnd_depth"
+        help = "Depth d of variable neighborhood search"
+        arg_type = Int
+        default = 1
+    "--sparse_evaluation"
+        help = "If set to false, the NN evaluates the vertices in each iteration. If set to true, the NN only evaluates "*
+               "the vertices in case no improving solution was found in the last iteration."
+        arg_type = Bool
+        default = false
+    "--runs_per_instance"
+        help = "Number of runs per instance. Output is averaged over all runs."
+        arg_type = Int
+        default = 1
+    "--neighborhood_size"
+        help = "Limit the size of sets X, Y of the restricted neighborhood to this size"
+        arg_type = Int
+        default = 20
+end
+
+function ArgParse.parse_item(::Type{Vector}, x::AbstractString)
+    return [(name="$(split(file, "/")[end])", graph=load_instance(file)) for file in split(x, ",")]
+end
+
+function get_stm(stm_string)
+    if stm_string == "tl"
+        return TabuList
+    elseif stm_string == "cc"
+        return ConfigurationChecking
+    else
+        error("unknown short term memory type")
+    end
+end
+
+scoring_function = nothing
+if settings[:scoring_function] != "-"
+    println("Load scoring function $(settings[:scoring_function])")
+    BSON.@load parsed_args["scoring_function"] gnn
+    scoring_function = Encoder_Decoder_ScoringFunction(gnn, settings[:neighborhood_size])
+end
